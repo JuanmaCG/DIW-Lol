0f3de7030ce4142894747e1f4a752e88e2a28c51
JuanmaCG <43745199+JuanmaCG@users.noreply.github.com>
2019-11-12 14:44:53 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -3,13 +3,17 @@ const vm = new Vue({
     data: {
         data: []
     },
-    mounted() {
+    mounted: function() {
         axios({
                 "method": "GET",
-                "url": "https://euw1.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key=RGAPI-5920c955-9c44-453c-973d-9d3fe81964af",
+                "url": "https://euw1.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key=RGAPI-4a957cc8-fcab-4621-93f6-7871642ba5ad",
+                "Origin": "https://developer.riotgames.com",
+                "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+                "X-Riot-Token": "RGAPI-4a957cc8-fcab-4621-93f6-7871642ba5ad",
+                "Accept-Language": "es-ES,es;q=0.9",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36"
 
-            })
-            .then((res) => {
+            }).then((res) => {
                 this.data = res.data.entries;
                 this.data.sort(function(b, a) {
                     if (a.leaguePoints > b.leaguePoints) {
--- js/user.js
+++ js/user.js
@@ -1,5 +1,5 @@
 const riotUrl = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/";
-const ApiKey = "RGAPI-5920c955-9c44-453c-973d-9d3fe81964af";
+const ApiKey = "RGAPI-4a957cc8-fcab-4621-93f6-7871642ba5ad";
 
 function buildUrl(username) {
     return riotUrl + username + "?api_key=" + ApiKey
@@ -22,20 +22,56 @@ const vm_user = new Vue({
     el: '#user',
     data: {
         usuario: findGetParameter('summonerName'),
-        data: []
+        userInfo: [],
+        leagueUserInfo: [],
+        userIconId: null,
+        iconUrl: null,
+        champInfo: [],
+        topFiveChamp: []
     },
-    mounted() {
+    mounted: function() {
         this.getUser(this.usuario);
+        
     },
     methods: {
         getUser(username) {
             let url = buildUrl(username);
             axios.get(url).then((response) => {
-                this.data = response;
+                this.userInfo = response.data;
+                this.userIconId = response.data.profileIconId;
+                this.iconUrl = "http://ddragon.leagueoflegends.com/cdn/9.22.1/img/profileicon/" + this.userIconId + ".png";
+                this.getWinsLosses(this.userInfo.id);
+                this.getChamps();
+                this.getTopFiveChamps(this.userInfo.id);
+                
             }).catch(error => { console.log(error); });
+        },
+        getWinsLosses(usernameId) {
+            axios.get('https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/' + usernameId + '?api_key=' + ApiKey)
+            .then((response) =>{
+                this.leagueUserInfo = response.data.find( leagueUserInfo => leagueUserInfo.queueType === "RANKED_SOLO_5x5");
+            }).catch(error => { console.log(error); });
+        },
+        getTopFiveChamps(usernameId) {
+            axios.get('https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/' + usernameId + '?api_key=' + ApiKey)
+            .then((response) => {
+                this.champInfo = response.data
+                
+            })
+        },
+        getChamps() {
+            axios.get('http://ddragon.leagueoflegends.com/cdn/9.22.1/data/en_US/champion.json')
+            .then((response) => {
+                
+            
+                // for(let i = 0; i < 5; i++) {
+                //     if(this.champInfo[i].championId == )
+                //     this.topFiveChamp[i] = 
+                    
+                // }
 
-
-
+                
+            })
         }
     }
 
